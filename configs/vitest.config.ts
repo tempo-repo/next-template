6d3ee28378792e31864a619286f50af3ff412920
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-13 13:50:26 +0200

    ⚙️ configure vitest

---
+++ configs/vitest.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPathsPlugin from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPathsPlugin()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'json-summary', 'html'],
+    },
+  },
+});
