88ec43052688e6f0f13250bdf7da3795f2821c6a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-13 13:37:26 +0200

    ✍️ reformat code

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'standalone'
+  output: 'standalone',
 };
 
 export default nextConfig;
