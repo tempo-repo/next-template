7dd709404abf0554979f36646960eba37582cb9b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-26 19:52:12 +0200

    🛡️ create constants namespace

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This namespace contains all project-wide constants.
+ */
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Constants {
+  export const APP_NAME = 'Next Template';
+  export const APP_DEFAULT_TITLE = 'My Awesome App';
+  export const APP_TITLE_TEMPLATE = `%s - ${Constants.APP_NAME}`;
+  export const APP_DESCRIPTION = 'Generated from next-template';
+}
