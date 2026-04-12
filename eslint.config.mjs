a226fc323f65dda2b2cfef31f7a11a937e5ac224
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-04-12 13:23:22 +0200

    ✍️ generate storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
@@ -13,6 +15,7 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
     'coverage',
+    'src/stories/**',
   ]),
   {
     rules: {
@@ -22,6 +25,7 @@ const eslintConfig = defineConfig([
       '@typescript-eslint/consistent-type-imports': 'error',
     },
   },
+  ...storybook.configs['flat/recommended'],
 ]);
 
 export default eslintConfig;
