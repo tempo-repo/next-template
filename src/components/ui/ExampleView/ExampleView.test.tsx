870c6168136236e39fb4ff3fb82cf5f2888861c4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-05-30 23:26:13 +0200

    🧪 test ExampleView

--- src/components/ui/ExampleView/ExampleView.test.tsx
+++ src/components/ui/ExampleView/ExampleView.test.tsx
@@ -1,10 +1,28 @@
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
 
 import { ExampleView } from './ExampleView';
 
 describe('ExampleView', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   test('It renders', () => {
     expect(() => render(<ExampleView />)).not.toThrow();
+  });
+
+  test('Buttons are working', () => {
+    // Render view
+    render(<ExampleView />);
+
+    // Extract buttons by their IDs
+    const lightThemeButton =
+      screen.getByTestId<HTMLButtonElement>('set-light-theme');
+    const darkThemeButton =
+      screen.getByTestId<HTMLButtonElement>('set-dark-theme');
+
+    expect(() => fireEvent.click(lightThemeButton)).not.toThrow();
+    expect(() => fireEvent.click(darkThemeButton)).not.toThrow();
   });
 });
--- src/components/ui/ExampleView/ExampleView.tsx
+++ src/components/ui/ExampleView/ExampleView.tsx
@@ -25,8 +25,19 @@ export const ExampleView: FC<ExampleViewProps & ExampleViewVariantsType> = ({
       >
         Hello, {message}!
         <div>
-          <button onClick={() => setTheme('light')}>Set light theme</button>
-          <button onClick={() => setTheme('dark')}>Set dark theme</button>
+          <button
+            data-testid={'set-light-theme'}
+            onClick={() => setTheme('light')}
+          >
+            Set light theme
+          </button>
+
+          <button
+            data-testid={'set-dark-theme'}
+            onClick={() => setTheme('dark')}
+          >
+            Set dark theme
+          </button>
         </div>
       </div>
     </>
