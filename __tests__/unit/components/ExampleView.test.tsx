44ae6c2d900f580aec0a86d2807c7006a1e7492d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-06-16 23:08:05 +0200

    📝 move last test file

--- __tests__/unit/components/ExampleView.test.tsx
+++ __tests__/unit/components/ExampleView.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { afterEach, describe, expect, test } from 'vitest';
 
-import { ExampleView } from './ExampleView';
+import { ExampleView } from '@/components/ui';
 
 describe('ExampleView', () => {
   afterEach(() => {
